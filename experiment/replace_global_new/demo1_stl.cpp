4f2c1fb2389d943576afd6730ccd6409c8f252ba
hexu <hexu_1985@sina.com>
2018-08-20 18:00:11 +0800

    update experiment/replace_global_new

--- experiment/replace_global_new/demo1_stl.cpp
+++ experiment/replace_global_new/demo1_stl.cpp
@@ -42,6 +42,39 @@ void operator delete(void *p)
 	return free(p);
 }
 
+void *operator new [](std::size_t size)
+{
+	void *pReturn = NULL;
+	printf("my operator new []\n");
+	if (size == 0)
+		size =1;	// 至少分配1字节
+
+	while (1) {
+		pReturn = malloc(size);
+		if (pReturn != NULL) {
+			printf("Got memory\n");
+			return pReturn;
+		}
+
+		new_handler globalhandler = set_new_handler(0);	// 找到新的new_handler
+		set_new_handler(globalhandler);
+
+		if (globalhandler) {
+			printf("call new_handler...\n");
+			(*globalhandler)();	// 如果内存不足了, 就调用new_handler
+		} else {
+			printf("no memory, no new_handler\n");
+			throw std::bad_alloc();
+		}
+	}
+}
+
+void operator delete [](void *p)
+{
+	printf("my operator delete []\n");
+	return free(p);
+}
+
 int main()
 {
     std::vector<int> vec(5);
